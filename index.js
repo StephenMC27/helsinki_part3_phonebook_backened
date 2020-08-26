47252b515d9d73ae122b03efbdfdd536904989a9
Stephen Clark <stephenmclark527@gmail.com>
2020-08-25 21:25:29 -0400

    Setup page to display persons data in JSON

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Dan Abramov",
+    number: "703-863-9393",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  },
+  {
+    name: "Mason Valentine",
+    number: "703-829-1717",
+    id: 7
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
