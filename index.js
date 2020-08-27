e61d9954eaad482c4b3d19349dd9ec09159ffb73
Stephen Clark <stephenmclark527@gmail.com>
2020-08-26 22:42:18 -0400

    Add functionality to fetch single person

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (!person) {
+    response.status(404).end()
+  }
+
+  response.json(person)
+})
+
 app.get('/info', (request, response) => {
   const date = new Date()
   response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`)
