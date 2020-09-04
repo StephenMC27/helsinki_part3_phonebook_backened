5fa50c3f8e74f3b2ac358da6302d8a23b75b5d96
Stephen Clark <stephenmclark527@gmail.com>
2020-09-03 22:08:19 -0400

    Added request console printing using morgan

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('content', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 persons = [
   {
@@ -59,7 +65,6 @@ const generateId = () => {
 
 app.post('/api/persons/', (request, response) => {
   const body = request.body
-  console.log(body)
 
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -79,8 +84,6 @@ app.post('/api/persons/', (request, response) => {
   }
   
   persons = persons.concat(person)
-
-  console.log(person)
 
   response.json(person)
 })
