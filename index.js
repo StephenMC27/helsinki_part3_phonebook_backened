a64927d4304d02cc1adbe965ccb447890687c301
Stephen Clark <stephenmclark527@gmail.com>
2020-09-04 23:45:18 -0400

    Add cors middleware

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 morgan.token('content', (request, response) => {
   return JSON.stringify(request.body)
 })
 
-app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 persons = [
